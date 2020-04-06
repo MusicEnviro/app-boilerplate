fa84a0004d5e373b7108fdd9e4385654e3b207a9
Geoffrey Holbrook <geof@Geoffreys-MacBook-Pro.local>
2020-04-05 20:18:55 -0400

    Updates

---
+++ precache-manifest.33153c5f75700df02388ea172e2f9f30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79bc2678e2d9fec94a8488547da667d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a9d895c29555b4b4e35",
+    "url": "/static/css/main.d10aaceb.chunk.css"
+  },
+  {
+    "revision": "fd1a046c1a308e620667",
+    "url": "/static/js/2.1876e775.chunk.js"
+  },
+  {
+    "revision": "2848759bb96b6b2b700b7bd477beceb1",
+    "url": "/static/js/2.1876e775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a9d895c29555b4b4e35",
+    "url": "/static/js/main.ec448a72.chunk.js"
+  },
+  {
+    "revision": "86a47c46e06ec21d2346",
+    "url": "/static/js/runtime-main.c4f92e67.js"
+  }
+]);
